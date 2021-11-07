6575b404cba7a830a4e7a45eb4f3164dc87ccac9
Sophia <sophkiani@gmail.com>
2021-11-07 10:24:45 -0600

    fixed bugs

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 ///////////////////////// Packages /////////////////////////
-const express = require("express");
+//NodeJS
+const fs = require("fs");
 const path = require("path");
-const api = require('./routes/index.js');
 
+// Express
+const express = require("express");
+
+// UUID 
+const { v4: uuidv4 } = require('uuid');
+
+//Linking database
+const dbJson = require('./db/db.json');
+
+//Defining Port
 const PORT = process.env.port || 3001;
 
 const app = express();
@@ -11,14 +21,54 @@ const app = express();
 ///////////////////////// Middle Ware /////////////////////////
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
 
+///////////////////////// Pages /////////////////////////
 // Returns a page
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "/public"))
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// Send to notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+)
+
+///////////////////////// GET Method /////////////////////////
+//Get home page
+app.get("/", (req, res) => {
+  res.json(dbJson);
+});
+
+// Get all of the old notes
+app.get("/notes", (req, res) => {
+  res.json(dbJson);
+});
+
+///////////////////////// Post Method /////////////////////////
+//Creates a new note
+app.post("/api/notes", (req, res) => {
+  req.body.id = uuidv4();
+  dbJson.push(req.body);
+
+  // Writes the updated variable to a JSON string
+  fs.writeFile('./db/db.json', JSON.stringify(dbJson), (err) =>{
+    err ? console.error(err) : console.log('YaY!')
+  });
+    res.status(201).json(dbJson)
+ });
+
+///////////////////////// Delete Method /////////////////////////
+//Deletes selected note
+app.delete("/api/notes/:id", (req, res) => {
+  dbJson = dbJson.filter (note => note.id != req.params.id);
+
+  fs.writeFile('./db/db.json', JSON.stringify(dbJson), (err) =>{
+    err ? console.error(err) : console.log('YaY')
+  });
+  res.status(201).json(dbJson)
 });
 
 
